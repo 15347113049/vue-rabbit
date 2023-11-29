f832a36a2c057f518f349b942343dab0d33908d6
zhuyang <1723929556@qq.com>
2023-11-29 11:02:24 +0800

    面板组件封装

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -3,15 +3,20 @@ import HomeCategory from "./components/HomeCategory.vue";
 import HomeBanner from "./components/HomeBanner.vue";
 import HomeNew from "./components/HomeNew.vue";
 import HomeHot from "./components/HomeHot.vue";
-import homeProduct from "./components/HomeProduct.vue";
+import HomeProduct from "./components/HomeProduct.vue";
+import HomePanel from "./components/HomePanel.vue";
 </script>
 
 <template>
   <div class="container">
     <HomeCategory />
     <HomeBanner />
+    <HomeNew />
+    <HomeHot />
+    <HomeProduct />
+    <!-- 测试面板组件 -->
+    <HomePanel title="新鲜好物">
+      <div>我是插槽</div>
+    </HomePanel>
   </div>
-  <HomeNew />
-  <HomeHot />
-  <homeProduct />
 </template>
