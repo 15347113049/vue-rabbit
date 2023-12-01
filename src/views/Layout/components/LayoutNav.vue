cfdac6f0674cec8247f06ec89ecae0f18f8b3b34
zhuyang <1723929556@qq.com>
2023-12-01 15:16:18 +0800

    登录-使用token适配多模板显示

--- src/views/Layout/components/LayoutNav.vue
+++ src/views/Layout/components/LayoutNav.vue
@@ -1,13 +1,18 @@
 <script setup>
+import { useUserStore } from "@/stores/user";
+const userStore = useUserStore();
 </script>
 
 <template>
   <nav class="app-topnav">
     <div class="container">
       <ul>
-        <template v-if="false">
+        <template v-if="userStore.userInfo.result.token">
           <li>
-            <a href="javascript:;"><i class="iconfont icon-user"></i>周杰伦</a>
+            <a href="javascript:;"
+              ><i class="iconfont icon-user"></i
+              >{{ userStore.userInfo.result.account }}</a
+            >
           </li>
           <li>
             <el-popconfirm
